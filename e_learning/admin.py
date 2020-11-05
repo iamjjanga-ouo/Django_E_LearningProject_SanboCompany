80aab64cb670e732d6caf8bd9a444d63618fe976
iamjjanga-ouo <namils147@kakao.com>
2020-11-06 08:17:43 +0900

    init admin.py

--- e_learning/admin.py
+++ e_learning/admin.py
@@ -1,3 +1,39 @@
 from django.contrib import admin
+from e_learning.models import Lecture, Assignment, Major, Professor, OpenLecture, Language
 
-# Register your models here.
+admin.site.register(Assignment)
+admin.site.register(Major)
+admin.site.register(Language)
+
+class LectureInline(admin.TabularInline):
+    model = Lecture
+    extra = 0
+
+@admin.register(Professor)
+class ProfessorAdmin(admin.ModelAdmin):
+    list_display = ('last_name', 'first_name', 'date_of_birth', 'major')
+    fields = ['first_name', 'last_name', 'date_of_birth', 'major']
+    inlines = [LectureInline]
+
+class OpenLectureInline(admin.TabularInline):
+    model = OpenLecture
+    extra = 0
+
+@admin.register(Lecture)
+class LectureAdmin(admin.ModelAdmin):
+    list_display = ('title', 'professor', 'major', 'language')
+    inlines = [OpenLectureInline]
+
+@admin.register(OpenLecture)
+class OpenLectureAdmin(admin.ModelAdmin):
+    list_display = ('lecture', 'status', 'enroll_student', 'remain_time', 'id')
+    list_filter = ('status', 'remain_time')
+
+    fieldsets = (
+        (None, {
+            'fields': ('lecture', 'id')
+        }),
+        ('Availability', {
+            'fields': ('status', 'remain_time', 'enroll_student')
+        }),
+    )
